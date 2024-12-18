76498ef69cf87e5811ab6c1ecec5ba58f2937727
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-18 13:59:26 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Button from './components/Button'
+
+
+function App() {
+
+  return (
+    <main>
+      <Button>Buy now!</Button>
+    </main>
+  )
+}
+
+export default App
